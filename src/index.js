1a0a546fb8592f5a9611e955ca63b173715f8eca
bubbledog333 <95395175+bubbledog333@users.noreply.github.com>
2022-06-09 19:27:28 -1000

    update

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <div className="App">
-  <div className="Class">
-  
-   <input/> <button><strong>Search</strong></button> <button><strong>Current</strong></button></div> 
-<div className="Api">
-<h1>Honolulu</h1>
-<p>Sunday 1:34pm</p><p>Sunny</p>
-<h2>81°F</h2>
-<p>Precipatation: 12%</p>
-<p >Wind: 2km/h</p>
-</div><ul className="days">
-<li className="ali">Mon</li><li className="ali">83°F</li>
-<li className="ali">Tue</li><li className="ali">85°F</li>
-<li className="ali">Wen</li><li className="ali">82°F</li>
-<li className="ali">Thur</li><li className="ali">79°F</li>
-<li className="ali">Fri</li><li className="ali">83°F</li>
-<li className="ali">Sat</li><li className="ali">86°F</li>
-<li className="ali">Sun</li><li className="ali">84°F</li>
-</ul>
-    </div>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
